fffd40e28ac5805ebfd9c5c7352ba1b4c9807ed1
unknown <juandiazpatino8@gmail.com>
2022-07-29 18:31:07 -0500

    make it better

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 'use strict'
 var mongoose=require('mongoose');
+var http=require('http');
 
 var app=require('./app');
 var port=3000;
+var server=http.createServer(app);
+
 mongoose.Promise=global.Promise;
+
 mongoose.connect('mongodb://localhost:27017/api_rest',{useNewUrlParser:true})
-        .then(()=>{
-            console.log("hola")
-            app.listen(port,()=>{
-                console.log("servidor corriendo en "+port);
-            })
-        });
+        .then(db=> console.log('db connect'))
+        .catch(error =>console.log(error));
+
+app.set('port', process.env.PORT||port);
+server.listen(app.get('port'),()=>{
+    console.log(`server on port ${app.get('port')}`);
+})
