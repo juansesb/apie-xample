1f8dd6be2eab5edc7608ae933f4dc44d4a5c962f
unknown <juandiazpatino8@gmail.com>
2022-07-29 19:09:11 -0500

    change

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var server=http.createServer(app);
 
 mongoose.Promise=global.Promise;
 /*'mongodb://localhost:27017/api_rest'*/
-mongoose.connect('mongodb+srv://juandiaz:amoonepiece@cluster0.emfmfbb.mongodb.net/test',{useNewUrlParser:true})
+mongoose.connect('mongodb+srv://juandiaz:amoonepiece@cluster0.emfmfbb.mongodb.net/api_rest',{useNewUrlParser:true})
         .then(db=> console.log('db connect'))
         .catch(error =>console.log(error));
 
