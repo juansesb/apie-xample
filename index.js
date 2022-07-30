9c25e08f47b309e7e6eff6737987337d4d7b628d
unknown <juandiazpatino8@gmail.com>
2022-07-29 19:56:08 -0500

    change

--- index.js
+++ index.js
@@ -6,11 +6,21 @@ var app=require('./app');
 var port=3000;
 var server=http.createServer(app);
 
-mongoose.Promise=global.Promise;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://juan:<amo@api.5qluf2r.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 /*'mongodb://localhost:27017/api_rest'*/
-mongoose.connect('mongodb+srv://juandiaz:amoonepiece@cluster0.emfmfbb.mongodb.net/api_rest',{useNewUrlParser:true})
+/*
+mongoose.Promise=global.Promise;
+
+mongoose.connect('mongosh "mongodb+srv://api.5qluf2r.mongodb.net/myFirstDatabase" --apiVersion 1 --username juan',{useNewUrlParser:true})
         .then(db=> console.log('db connect'))
-        .catch(error =>console.log(error));
+        .catch(error =>console.log(error));*/
 
 app.set('port', process.env.PORT||port);
 server.listen(app.get('port'),()=>{
